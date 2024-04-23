e24feb4a14059ae671e91b5b931a9bd58490b9c1
Riegel, Celina <celina.riegel@dlr.de>
2024-04-23 16:38:19 +0200

    separat header

---
+++ js/include-header.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.getElementById("header-placeholder").innerHTML = xhr.responseText;
+            } else {
+                console.error('Error loading header:', xhr.status);
+            }
+        }
+    };
+    xhr.open("GET", "partial/header.html", true);
+    xhr.send();
+});
