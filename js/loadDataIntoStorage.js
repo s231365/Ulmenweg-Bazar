3882b0ba1592dfe44f237c7352b8d65a5ab01011
paulseidel08 <s231363@student.dhbw-mannheim.de>
2024-05-21 16:12:33 +0200

    The code now only loads the localStorage once in home.html and only if theres no data already

--- js/loadDataIntoStorage.js
+++ js/loadDataIntoStorage.js
@@ -6,9 +6,11 @@ function loadDataFromJSON() {
             const jsonDataString = JSON.stringify(data);
 
             // Speichere den JSON-String im Local Storage unter dem Schlüssel 'products'
-            localStorage.setItem('products', jsonDataString);
 
-            console.log('Daten erfolgreich im Local Storage gespeichert:', data);
+            if(localStorage.getItem('products') === null) {
+                localStorage.setItem('products', jsonDataString);
+                console.log('Daten erfolgreich im Local Storage gespeichert:', data);
+            }
         })
         .catch(error => console.error('Fehler beim Laden der JSON-Datei:', error));
 
