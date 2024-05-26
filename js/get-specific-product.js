fee2490be02dc9cd5b62cea4c1d95b205db36df7
paulseidel08 <s231363@student.dhbw-mannheim.de>
2024-05-26 14:36:28 +0200

    Clean filteredProducts.html and product.html with outsourcing scripts

---
+++ js/get-specific-product.js
@@ -0,0 +1,16 @@
+// get the id given in the URL
+const urlParams = new URLSearchParams(window.location.search);
+const productId = urlParams.get('id');
+
+// get the string value from local storage and filter for a key
+const all_data = JSON.parse(localStorage.getItem('products'));
+const products = {};
+
+for (const key in all_data.products) {
+    const element = all_data.products[key];
+    const element_id = element.id;
+    if (element_id === productId) { // Checking if the key is equal to productId
+        products[key] = element;
+    }
+}
+const filtered_data = {products: products}
--- js/sortingFunctions.js
+++
@@ -1,7 +0,0 @@
-function reloadPageForSorting(sortingString) {
-    let url = new URL(window.location.href);
-    let params = new URLSearchParams(url.search);
-    params.set('sorting', sortingString);
-    url.search = params.toString();
-    window.location.href = url.toString();
-}
