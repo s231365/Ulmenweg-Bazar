563047dbf950f3104c4efa3c90f79b23627e0311
paulseidel08 <s231363@student.dhbw-mannheim.de>
2024-06-04 16:54:21 +0200

    Render Function of Admin also in renderFunctions.js

--- js/adminAccessFunctions.js
+++ js/adminAccessFunctions.js
@@ -47,3 +47,26 @@ function validate(form, event) {
         createItem(form);
     }
 }
+
+function deleteItem(itemId) {
+    let productsString = localStorage.getItem('products');
+
+    // Parse the JSON string back into a JavaScript object
+    let products = JSON.parse(productsString);
+
+    // Convert the products object into an array of key-value pairs
+    let productsArray = Object.entries(products);
+
+    // Filter out the item to delete
+    let updatedProductsArray = productsArray.filter((item, index) => index !== parseInt(itemId));
+
+    // Convert the updated array back into an object
+    let updatedProducts = Object.fromEntries(updatedProductsArray);
+
+    // Update the products in local storage
+    localStorage.setItem('products', JSON.stringify(updatedProducts));
+
+    // Reload the cart section to reflect the changes
+    location.reload();
+
+}
